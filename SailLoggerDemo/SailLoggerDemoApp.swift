29ab608023e9942ca31e59c5169e1c3465b98769
Bear <164508906@qq.com>
2023-12-29 17:06:54 +0800

    Initial Commit

---
+++ SailLoggerDemo/SailLoggerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SailLoggerDemoApp.swift
+//  SailLoggerDemo
+//
+//  Created by 李招雄 on 2023/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SailLoggerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
